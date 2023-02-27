412457092c780cffc248daf57b35f92720cf0fe0
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-02-26 22:57:03 -0500

    working on update employee role function

--- db/schema.sql
+++ db/schema.sql
@@ -17,6 +17,7 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     salary INT NOT NULL,
+    INDEX department_index(department_id),
     department_id INT,
     is_manager BOOLEAN NOT NULL, 
     FOREIGN KEY (department_id)
@@ -30,7 +31,12 @@ CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
+    INDEX manager_index(manager_id),
     manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL,
+    INDEX role_index (role_id),
     role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,23 +7,26 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id, is_manager)
 VALUES
-("Salesperson", 100000, 1, false),
 ("Sales Lead", 200000, 1, true),
-("Lawyer", 225000, 4, false),
+("Salesperson", 100000, 1, false),
 ("Legal Team Lead", 250000, 4, true),
-("Software Engineer", 100000, 2, false),
+("Lawyer", 225000, 4, false),
 ("Lead Engineer", 150000, 2, true),
-("Accountant", 140000, 3, false),
-("Account Manager", 160000, 3, true);
+("Software Engineer", 100000, 2, false),
+("Account Manager", 160000, 3, true),
+("Accountant", 140000, 3, false);
+
 
 
 INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUES
-("Stephanie", "Wilkins", 2, 1),
+
 ("Tammy", "Troxler", null, 2),
-("Mila", "Jones", 4, 3),
+("Stephanie", "Wilkins", 2, 1),
 ("Tonya", "Stephens", null, 4),
-("Leah", "Traynor", 6, 5),
+("Mila", "Jones", 4, 3),
 ("Andy", "Fox", null, 6),
-("Viola", "Martin", 8, 7),
-("Todd", "Bashore", null, 8);
+("Leah", "Traynor", 6, 5),
+("Todd", "Bashore", null, 8),
+("Viola", "Martin", 8, 7);
+
