b729d3c8594ce3b6b47fe6d96138d534add63097
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-02-28 12:20:48 -0500

    minor changes

--- index.js
+++ index.js
@@ -294,17 +294,3 @@ async function findEmployees() {
 }
 
 initiateProgram()
-
-/* Bonus
-Update employee managers.
-
-View employees by manager.
-
-View employees by department.
-
-Delete departments, roles, and employees.
-
-View the total utilized budget of a department—in other words, the combined salaries of all employees in that department. */
-
-
-
